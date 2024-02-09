972fa9d84b091ad9733a2e0c1477458be7e7d9ef
laznarb <aa22.laznar44@iespabloserrano.com>
2024-02-09 14:05:12 +0100

    me quiero morir

--- Trabajo_js/script.js
+++ Trabajo_js/script.js
@@ -1,11 +1,30 @@
 //Cogemos el enlace a la API
-const enlaceApi = "https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/all-vehicles-model/records?limit=50";
-let coche = document.getElementById("coche");
+const Api_inicio = "https://gateway.marvel.com/v1/public/";
+const Api_characters = "characters";
+const Api_APIKEY = "?ts=1000&apikey=26df109c2dadc2d98d71712ff2cd13d1&hash=c60feaebf8ac04d3cbc6327cc1c06fb1";
+const personaje = document.getElementById("personaje");
+
+
+
 //Llamamos al enlace
-fetch(enlaceApi) //Aquí se llama a la URL
-.then(function(response){
-    return response.json(); //Cuando responde se formatean las respuestas a JSON
-})
-.then (function(json){
-    console.log(json)
-}); 
+fetch(Api_inicio+Api_characters+Api_APIKEY) //Aquí se llama a la URL
+    .then(function(response){
+            return response.json(); //Cuando responde se formatean las respuestas a JSON
+    })
+    .then (function(json){
+            //console.log(json);
+            console.log(json.data.results);
+
+            let personajes = json.data.results;
+            let nombrePersonajes = "";
+            let imagenPersonajes = "";
+            for (let i=0; i<personajes.length; i++) {
+                nombrePersonajes += "<div>"
+                nombrePersonajes += "<h2>"+personajes[i].name+"</h2>";
+                    let imagen = personajes[i].thumbnail.path + "." + personajes[i].thumbnail.extension;
+                    imagenPersonajes = "<img src='"+imagen+"' >";
+                nombrePersonajes += imagenPersonajes;
+                nombrePersonajes += "</div>";
+            }
+            personaje.innerHTML = nombrePersonajes;
+    }); 
